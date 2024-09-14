e1d48a5ce12ff379765376495895b08ac1498c43
Rere <rereisforever@gmail.com>
2024-09-15 03:08:14 +1000

    Charities GUI: 3 charities and their info. + log out button. Done.

--- src/main/java/com/example/finalassignmentcab302/CharitiesPageController.java
+++ src/main/java/com/example/finalassignmentcab302/CharitiesPageController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.*;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 
+import java.io.IOException;
+
 public class CharitiesPageController {
 
     private Stage primaryStage; // Add a Stage field
@@ -14,14 +16,45 @@ public class CharitiesPageController {
 
     @FXML
     private Label lblCharity1;
-
     @FXML
     private Text txtCharity1;
+    @FXML
+    private Button btnCharity1;
+
+    @FXML
+    private Label lblCharity2;
+    @FXML
+    private Text txtCharity2;
+    @FXML
+    private Button btnCharity2;
+
+    @FXML
+    private Label lblCharity3;
+    @FXML
+    private Text txtCharity3;
+    @FXML
+    private Button btnCharity3;
+
+    @FXML
+    private Button btnLogout;
 
     @FXML
     private void initialize() {
         lblCharity1.setText("Charity 1 Name.");
         txtCharity1.setText("Charity 1 description! Very very long.");
+
+        lblCharity2.setText("Charity 2 Name.");
+        txtCharity2.setText("Charity 2 description! Very very long.");
+
+        lblCharity3.setText("Charity 3 Name.");
+        txtCharity3.setText("Charity 3 description! Very very long.");
+    }
+
+    @FXML
+    private void handleOpenHome() throws IOException {
+        Stage stage = (Stage) btnLogout.getScene().getWindow(); // Get the current stage
+        HelloApplication app = new HelloApplication();
+        app.start(stage); // Switch to the new page
     }
 
 }
