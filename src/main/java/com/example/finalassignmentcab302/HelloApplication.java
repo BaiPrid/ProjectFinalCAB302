0e799110e8366b2c0720a5dac584f63d69e401e5
Rere <rereisforever@gmail.com>
2024-09-17 17:05:13 +1000

    Make landing page default page

--- src/main/java/com/example/finalassignmentcab302/HelloApplication.java
+++ src/main/java/com/example/finalassignmentcab302/HelloApplication.java
@@ -17,7 +17,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LandingPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 500);
         stage.setTitle("Home Page!");
         stage.setScene(scene);
