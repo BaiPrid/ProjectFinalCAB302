ea87d39ca59ff4d24bc4e895d909203cc7f0d28d
BaiPrid <baileyprid21@gmail.com>
2024-10-07 16:35:43 +1000

    Updating Question Object and Changing Questions

--- src/main/java/com/example/finalassignmentcab302/StaticQuestionList.java
+++ src/main/java/com/example/finalassignmentcab302/StaticQuestionList.java
@@ -8,9 +8,12 @@ public class StaticQuestionList {
     private static final List<Question> questions = new ArrayList<>();
 
     static {
-        questions.add(new Question("Would you rather donate to a charity that helps people or animals", "People", "Animals"));
-        questions.add(new Question("Would you rather donate a lot of money or a little bit of money", "A lot", "A little"));
-        questions.add(new Question("Would you rather donate to a large charity or a small charity", "Large", "Small"));
+        questions.add(new Question("What type of charity would you like support?", "Animals", "Orphans", "Environmental","Cancer Patients", "Local Schools", "Poor Countries", "Homeless"));
+        questions.add(new Question("Would you rather donate to a for-profit or not-for-profit organisation?", "For Profit", "Not For Profit"));
+        questions.add(new Question("What size charity would you like to donate to?", "Local", "Small", "Medium", "Large", "National", "Global"));
+        questions.add(new Question("How would you best like the charity to be run?", "Monetary Donation", "Volunteer Work", "Payed Employees", "Hired 3rd Party Corporations"));
+        questions.add(new Question("Would you like your donations to be tax deductible?", "Tax Deductible", "Not Tax Deductible"));
+        questions.add(new Question("How much control would you like over your donation?", "You Choose", "Organisation Chooses"));
     }
 
     public static List<Question> getQuestions() {
