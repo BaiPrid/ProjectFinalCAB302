f212f0feac6ce18877a441df34dd0603f2456299
BaiPrid <baileyprid21@gmail.com>
2024-10-15 19:22:55 +1000

    Adding javadoc comments to question page controller

--- src/main/java/com/example/finalassignmentcab302/Controllers/QuestionPageController.java
+++ src/main/java/com/example/finalassignmentcab302/Controllers/QuestionPageController.java
@@ -15,14 +15,24 @@ import java.util.List;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+/**
+ * Controller for the question page. Displays 5 questions to the user and stores their answers in a database
+ */
 public class QuestionPageController {
 
 
+    /**
+     * Keeps track of the current question the user is completing
+     */
     private int questionIncrement = 0;
-    private int questionVal = 0;
-    List<Integer> answerList = new ArrayList<>();
+    /**
+     * Holds the list of answers the user enters before uploading it to the database
+     */
     List<String> answerList2 = new ArrayList<>();
 
+    /**
+     * Contains the list of questions and their respective answers
+     */
     List<Question> questions = StaticQuestionList.getQuestions();
 
     @FXML
@@ -52,6 +62,11 @@ public class QuestionPageController {
     @FXML
     private ComboBox UserAnswersField;
 
+    /**
+     * Grabs the list of questions and displays the current question to the user
+     * in accordance with the questionIncrement value which updates after every
+     * user entry
+     */
     @FXML
     private void initialize() {
 
@@ -60,12 +75,18 @@ public class QuestionPageController {
         displayQuestion(questions.get(questionIncrement));
     }
 
+    /**
+     * Method for clearing the users current selection and replacing all values in the combobox.
+     * Once values are cleared, this method changes the question shown to the user and inserts
+     * all of that questions answers into the combobox
+     * @param question the object containing the question itself and its answers
+     */
     //Stores the answers for each question inside an answer list
     private void displayQuestion(Question question) {
         questionField.setText(question.getQuestion());
 
-        UserAnswersField.getSelectionModel().clearSelection();
-        UserAnswersField.getItems().clear();
+        UserAnswersField.getSelectionModel().clearSelection(); //clears the user's selection
+        UserAnswersField.getItems().clear(); //clears all items from the answerfield
 
         List<String> answers = new ArrayList<>();
 
@@ -96,6 +117,11 @@ public class QuestionPageController {
 
     }
 
+    /**
+     * Sends the user to the charities page which displays charities that would be of the highest interest
+     * to the user
+     * @throws IOException if there is a problem loading the page
+     */
     @FXML
     private void handleCharitiesPage() throws IOException {
         Stage stage = (Stage) nextButton.getScene().getWindow(); // Get the current stage
@@ -106,7 +132,12 @@ public class QuestionPageController {
         stage.setScene(scene);
     }
 
-
+    /**
+     * This method is responsible for the core functionality of the page.
+     * Once the user clicks the next button, they are presented with the next question in the list of
+     * questions so long as they have provided an answer, otherwise they are shown an error.
+     * @throws IOException if there is a problem loading the page
+     */
     @FXML
     private void onNextButtonClick() throws IOException {
 
@@ -120,8 +151,12 @@ public class QuestionPageController {
             errorField.setText("Please Choose an Answer!");
         }
 
-        //questionIncrement++;
-
+        /**
+         * Goes through the questions shown to the user until there are no questions remaining in the list.
+         * After the final question, all stored values are sent to the database with an UPDATE query.
+         * This query is UPDATE rather than INSERT as some users may be redoing their answers and this ensures
+         * that no duplicate entries are entered.
+          */
         if (questionIncrement <= 4){
             displayQuestion(questions.get(questionIncrement));
         }
@@ -142,7 +177,9 @@ public class QuestionPageController {
     }
 
 
-
+    /**
+     * Disables the next button by default until an option is selected
+     */
     @FXML
     private void activateNextButton(){
         boolean accepted = radButton1.isSelected() || radButton2.isSelected();
