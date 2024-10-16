e1cc5d25558686cab39d596e91ae57bc2052ec66
Rere <rereisforever@gmail.com>
2024-10-17 09:36:36 +1000

    Minor change + Amare javadoc

--- src/main/java/com/example/finalassignmentcab302/Controllers/UserAccountsController.java
+++ src/main/java/com/example/finalassignmentcab302/Controllers/UserAccountsController.java
@@ -3,18 +3,17 @@ package com.example.finalassignmentcab302.Controllers;
 import com.example.finalassignmentcab302.HelloApplication;
 import com.example.finalassignmentcab302.Tables.Order;
 import com.example.finalassignmentcab302.Tables.User;
-import com.example.finalassignmentcab302.dao.OrganisationDAO;
 import com.example.finalassignmentcab302.dao.UserDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import com.example.finalassignmentcab302.dao.UserDAO;
 import com.example.finalassignmentcab302.dao.OrderDAO;
@@ -30,21 +29,13 @@ public class UserAccountsController
     @FXML
     private Button btnDonate;
     @FXML
-    private Button btnBack;
+    private Button btnLogout;
     @FXML
     private Button btnQuestions;
     @FXML
     private Label txtTitle;
     @FXML
     public ListView<Order> orderListView;
-    @FXML
-    private TextField orderIDField;
-    @FXML
-    private TextField organisationField;
-    @FXML
-    private TextField orderDateField;
-    @FXML
-    private TextField amountField;
 
 
     //---------- BUTTONS TO DIFFERENT PAGES !! -------------
@@ -55,7 +46,7 @@ public class UserAccountsController
      */
     @FXML
     private void handleOpenHome() throws IOException {
-        Stage stage = (Stage) btnBack.getScene().getWindow(); // Get the current stage
+        Stage stage = (Stage) btnLogout.getScene().getWindow(); // Get the current stage
         HelloApplication app = new HelloApplication();
         app.start(stage); // Switch to the new page
     }
@@ -89,89 +80,30 @@ public class UserAccountsController
         stage.setScene(scene);
     }
 
-    // ------------- DATA INTERACTION -------------
-    UserDAO userNameDAO = new UserDAO();
-    OrderDAO userOrdersDAO = new OrderDAO();
-    OrganisationDAO orgDAO = new OrganisationDAO();
-
-    int userId = currentUser; // Replace with actual user ID
-    // Gets the answers for the specific user id
-    String userName = userNameDAO.getName(userId);
 
     /**
-     * Initialising the FXML fx:id values.
+     * Retrieving the user's past order data.
      */
     @FXML
     private void initialize()
     {
-        txtTitle.setText("Welcome " + userName + "!");
+        // ------------- DATA INTERACTION -------------
+        UserDAO userNameDAO = new UserDAO();
+        OrderDAO userOrdersDAO = new OrderDAO();
 
-        orderListView.setCellFactory(this::renderCell);
-        syncOrders();
-        // Select the first order and display order information
-        orderListView.getSelectionModel().selectFirst();
-        Order firstorder = orderListView.getSelectionModel().getSelectedItem();
-        if (firstorder != null) {
-            selectOrder(firstorder);
-        }
-        
-    }
+        int userId = currentUser; // Replace with actual user ID
+        // Gets the answers for the specific user id
 
-    /**
-     * Synchronizes the orders list view with the orders in the database.
-     */
-    private void syncOrders() {
-        orderListView.getItems().clear();
+        User userName = userNameDAO.getByLogin(userId);
+        String name = userName.getFirstName();
+
         List<Order> userOrders = userOrdersDAO.getUserOrders(userId);
+
         boolean hasOrders = !userOrders.isEmpty();
         if (hasOrders) {
             orderListView.getItems().addAll(userOrders);
+            txtTitle.setText("Welcome " + name);
         }
-    }
-
-    /**
-     * Programmatically selects an order in the list view and
-     * updates the text fields with the order's information.
-     * @param order The order to select.
-     */
-    private void selectOrder(Order order) {
-        orderListView.getSelectionModel().select(order);
-        orderIDField.setText(Integer.toString(order.getOrderId()));
-        organisationField.setText(orgDAO.getName(order.getOrganisationId()));
-        orderDateField.setText(order.getOrderDateTime());
-        amountField.setText(Float.toString(order.getAmount()));
-    }
-
-    private ListCell<Order> renderCell(ListView<Order> orderListView) {
-        return new ListCell<>() {
-            /**
-             * Handles the event when an order is selected in the list view.
-             * @param mouseEvent The event to handle.
-             */
-            private void onOrderSelected(MouseEvent mouseEvent) {
-                ListCell<Order> clickedCell = (ListCell<Order>) mouseEvent.getSource();
-                // Get the selected order from the list view
-                Order selectedOrder = clickedCell.getItem();
-                if (selectedOrder != null) selectOrder(selectedOrder);
-            }
-
-            /**
-             * Updates the item in the cell by setting the text to the contact's full name.
-             * @param order The order to update the cell with.
-             * @param empty Whether the cell is empty.
-             */
-            protected void updateItem(Order order, boolean empty) {
-                super.updateItem(order, empty);
-                // If the cell is empty, set the text to null, otherwise set it to the contact's full name
-                if (empty || order == null || order.getAmount() == null) {
-                    setText(null);
-                    super.setOnMouseClicked(this::onOrderSelected);
-                } else {
-                    setText(Float.toString(order.getAmount()));
-                }
-            }
-
-        };
     }
 
 
